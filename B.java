977f25b2a343594c608edb143b5b3708a2f085a9
Rani Ingole <raniingole497@gmail.com>
2022-12-06 11:23:22 +0530

    create substraction class committed..

---
+++ B.java
@@ -0,0 +1,17 @@
+public class Substraction
+{
+   int a;
+   int b;
+
+ public Substraction(int a, int b)
+{
+   this.a=a;
+   this.b=b;
+   System.out.println(a-b)
+}
+ public static void main(String args[])
+   {
+        Substraction s=new Substraction(20,10,);
+        
+   }
+}
